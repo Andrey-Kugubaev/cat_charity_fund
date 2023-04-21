19f149653a2f458aed94ad98726484688f9d36ca
Andrey Kugubaev <andrey.kugubaev@yandex.ru>
2023-04-21 22:58:46 +0300

    ver 1.03

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     app_title: str = 'Donation service'
     description: str = 'Donation service for cats'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    secret: str = 'UgFdnvDcSD',
+    secret: str = 'UgFdnvDcSD'
     first_superuser_email: Optional[EmailStr] = 'andrey@ya.ru'
     first_superuser_password: Optional[str] = 'parolkarol'
 
